8abc30065c514a848da6a26f176a613e16c171d3
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-07-16 17:45:38 +0900

    feat(#24) : FCM 세팅 완료
    S11P12D109-24

--- Colorpl-Android/core/src/main/java/com/colorpl/ColorplApplication.kt
+++ Colorpl-Android/core/src/main/java/com/colorpl/ColorplApplication.kt
@@ -2,11 +2,13 @@ package com.colorpl
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class ColorplApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
 
     }
 }
--- Colorpl-Android/presentation/src/main/java/com/presentation/MainActivity.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/MainActivity.kt
@@ -6,18 +6,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.colorpl.presentation.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // FCM SDK 초기화
+        FirebaseApp.initializeApp(this);
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task: Task<String> ->
+                val token = task.result
+                Timber.d("FCM Log ${token}")
+                if (!task.isSuccessful) {
+                    Timber.d("호요요")
+                    return@addOnCompleteListener
+                }
+
+            }
+
+
     }
 }
