a7cb4ab4cb7449b7cb27c6ffd26374214df3e12f
Mingyu Kim <mingyu5675@gmail.com>
2024-07-17 16:57:11 +0900

    feat(#S11P12D109-26): 리소스 추가, 바텀네티 세팅 폰트, 아이콘, 빈 fragment들

--- Colorpl-Android/presentation/src/main/java/com/presentation/MainActivity.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.presentation
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.colorpl.presentation.R
+import com.colorpl.presentation.databinding.ActivityMainBinding
 import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
+import com.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
+    private lateinit var navController: NavController
+
+    override fun init() {
+        initBottomNavBar()
+        initFCM()
+    }
+
+    private fun initBottomNavBar(){
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigationBar.setupWithNavController(navController)
+        binding.bottomNavigationBar.setOnItemReselectedListener {
+
+        }
+    }
+
+    private fun initFCM(){
         // FCM SDK 초기화
         FirebaseApp.initializeApp(this);
 
@@ -30,7 +50,5 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
-
-
     }
 }
