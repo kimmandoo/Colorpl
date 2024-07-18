811d7909bbd2a589c75318765ff9cdff5533a697
WOONBE <kepull2918@naver.com>
2024-07-18 10:55:53 +0900

    S11P12D109-32 feat(#S11P12D109-32) : 기본 코드 작성

---
+++ Colorpl-Backend/src/main/java/com/D109/Colorpl_Backend/ColorplBackendApplication.java
@@ -0,0 +1,13 @@
+package com.D109.Colorpl_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorplBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorplBackendApplication.class, args);
+	}
+
+}
---
+++ Colorpl-Backend/src/test/java/com/D109/Colorpl_Backend/ColorplBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.D109.Colorpl_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorplBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
