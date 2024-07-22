fdce3491aaeaeb32a37a703aff31d7459963bf79
김민규 <mingyu5675@gmail.com>
2024-07-22 23:28:59 +0900

    feat(#S11P12D109-179): 피드 상세페이지를 viewType으로 나눠서 한 리사이클러뷰로 구현

--- Colorpl-Android/presentation/src/main/java/com/presentation/feed/FeedDetailFragment.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/feed/FeedDetailFragment.kt
@@ -3,14 +3,16 @@ package com.presentation.feed
 import com.colorpl.presentation.R
 import com.colorpl.presentation.databinding.FragmentFeedDetailBinding
 import com.domain.model.Comment
+import com.domain.model.Feed
 import com.presentation.base.BaseFragment
-import com.presentation.component.adapter.feed.CommentAdapter
+import com.presentation.component.adapter.feed.FeedDetail
+import com.presentation.component.adapter.feed.FeedDetailAdapter
 import java.util.Date
 
 class FeedDetailFragment : BaseFragment<FragmentFeedDetailBinding>(R.layout.fragment_feed_detail) {
 
-    private val commentAdapter by lazy {
-        CommentAdapter(
+    private val feedDetailAdapter by lazy {
+        FeedDetailAdapter(
             onEditClickListener = { onEditClickListener() },
             onDeleteClickListener = { onDeleteClickListener() }
         )
@@ -18,22 +20,37 @@ class FeedDetailFragment : BaseFragment<FragmentFeedDetailBinding>(R.layout.frag
 
     override fun initView() {
         binding.apply {
-            rvComment.adapter = commentAdapter
-
-
-            val testComment = mutableListOf<Comment>()
+            rvFeedDetail.adapter = feedDetailAdapter
+            val testComment = mutableListOf<FeedDetail>()
+            testComment.add(
+                FeedDetail.HEADER(
+                    Feed(
+                        feedId = 1138,
+                        title = "sed",
+                        userName = "George Foster",
+                        userProfileImg = null,
+                        contentImg = null,
+                        emotionMode = "definitionem",
+                        emotionTotal = 1729,
+                        commentTotal = 8645,
+                        uploadedDate = Date()
+                    )
+                )
+            )
             repeat(10) {
                 testComment.add(
-                    Comment(
-                        commentId = 5906,
-                        name = "Roscoe Bowman",
-                        uploadDate = Date(),
-                        lastEditDate = null,
-                        content = "eruditi"
+                    FeedDetail.BODY(
+                        Comment(
+                            commentId = 5906,
+                            name = "Roscoe Bowman",
+                            uploadDate = Date(),
+                            lastEditDate = null,
+                            content = "eruditi"
+                        )
                     )
                 )
             }
-            commentAdapter.submitList(testComment)
+            feedDetailAdapter.submitList(testComment)
         }
     }
 
