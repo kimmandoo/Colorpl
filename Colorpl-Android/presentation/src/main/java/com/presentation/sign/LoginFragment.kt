518db3fd9aaada6640a3dd1661829afe07c2e5a0
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-07-22 10:46:33 +0900

    ui(#S11P12D109-55) : 로그인 navigation

---
+++ Colorpl-Android/presentation/src/main/java/com/presentation/sign/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.presentation.sign
+
+import com.colorpl.presentation.R
+import com.colorpl.presentation.databinding.FragmentLogInBinding
+import com.presentation.base.BaseFragment
+
+
+class LoginFragment : BaseFragment<FragmentLogInBinding>(R.layout.fragment_log_in) {
+    override fun initView() {
+
+    }
+}
