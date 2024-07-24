f8881b74dc79f036a4bb98435c38530e43390da6
박민희 <minuteman0212@naver.com>
2024-07-24 13:56:10 +0900

    S11P12D109-154 feat: 공연회차 Repository 작성

---
+++ Colorpl-Backend/src/main/java/com/colorpl/show/domain/schedule/domain/ShowScheduleRepository.java
@@ -0,0 +1,6 @@
+package com.colorpl.show.domain.schedule.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowScheduleRepository extends JpaRepository<ShowSchedule, Integer> {
+}
