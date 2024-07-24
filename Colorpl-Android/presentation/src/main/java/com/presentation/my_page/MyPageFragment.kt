198bec9d08550738b0defd1c54dbd143604b3e2d
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-07-24 15:06:38 +0900

    ui(#S11P12D109-73) : 마이페이지 메인 UI 완료

--- Colorpl-Android/presentation/src/main/java/com/presentation/my_page/MyPageFragment.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/my_page/MyPageFragment.kt
@@ -3,29 +3,87 @@ package com.presentation.my_page
 import android.animation.ObjectAnimator
 import com.colorpl.presentation.R
 import com.colorpl.presentation.databinding.FragmentMyPageBinding
+import com.domain.model.Ticket
 import com.presentation.base.BaseFragment
+import com.presentation.component.adapter.schedule.TicketAdapter
+import com.presentation.util.dpToPxF
+import com.presentation.util.setDistanceX
+import com.presentation.util.setTransactionX
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Date
 
 @AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
-    override fun initView() {
-        initClickEvent()
+    private val ticketAdapter : TicketAdapter by lazy {
+        TicketAdapter(
+            onTicketClickListener = {
+
+            }
+        )
     }
 
 
-    private fun initClickEvent(){
+    override fun initView() {
+        initTicket()
+        initClickEvent()
+    }
+
+    private fun initTicket(){
+        binding.rcTicket.adapter = ticketAdapter
+
+        ticketAdapter.submitList(
+            listOf( // testcode
+                Ticket(
+                    ticketId = 4706,
+                    name = "Elijah Merritt",
+                    date = Date(),
+                    space = "ignota",
+                    seat = "commune"
+                ),
+                Ticket(
+                    ticketId = 4706,
+                    name = "Elijah Merritt",
+                    date = Date(),
+                    space = "ignota",
+                    seat = "commune"
+                ),
+                Ticket(
+                    ticketId = 4706,
+                    name = "Elijah Merritt",
+                    date = Date(),
+                    space = "ignota",
+                    seat = "commune"
+                ),
+                Ticket(
+                    ticketId = 4706,
+                    name = "Elijah Merritt",
+                    date = Date(),
+                    space = "ignota",
+                    seat = "commune"
+                )
+            )
+        )
+
+    }
+
+
+    private fun initClickEvent() {
         val star = binding.ivTicketStar
         val expire = binding.ivTicketExpire
         star.isSelected = true
+
         binding.ivTicketStar.setOnClickListener {
             it.isSelected = !it.isSelected
-            expire.isSelected = !it.isSelected
+            expire.isSelected = false
+            binding.indicator.setTransactionX(0f)
         }
 
         binding.ivTicketExpire.setOnClickListener {
             it.isSelected = !it.isSelected
-            star.isSelected = !it.isSelected
+            star.isSelected = false
+            val distance = setDistanceX(binding.ivTicketStar, binding.ivTicketExpire)
+            binding.indicator.setTransactionX(distance)
         }
     }
 
