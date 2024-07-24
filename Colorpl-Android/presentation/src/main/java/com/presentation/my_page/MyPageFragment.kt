472367732e37c669865d405d5ed2c4d64bc0c7a1
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-07-24 11:36:11 +0900

    ui(#S11P12D109-73) : mypage ui 반

--- Colorpl-Android/presentation/src/main/java/com/presentation/my_page/MyPageFragment.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/my_page/MyPageFragment.kt
@@ -3,7 +3,9 @@ package com.presentation.my_page
 import com.colorpl.presentation.R
 import com.colorpl.presentation.databinding.FragmentMyPageBinding
 import com.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun initView() {
 
