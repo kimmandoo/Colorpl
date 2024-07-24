2ee3013ab0fb24e49d39c6d3497618ac185c51b9
구동현 <dies5115@naver.com>
2024-07-24 17:04:31 +0900

    Revert "revert feat(#S11P12D109-173): 리뷰 삭제 및 변경 구현"
    This reverts commit 11afc7b269811ca6d766e37c52a64f5e6c1176b3, reversing
    changes made to 135e7fc171d2d923dd82e5e2dedc5fe97a52a3f6.

--- Colorpl-Backend/src/main/java/com/colorpl/comment/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.colorpl.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-}
--- Colorpl-Backend/src/main/java/com/colorpl/comment/CommentService.java
+++
@@ -1,4 +0,0 @@
-package com.colorpl.comment;
-
-public class CommentService {
-}
