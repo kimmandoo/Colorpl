5c81758124a315ad453debab69faec197a1b760d
Mingyu Kim <mingyu5675@gmail.com>
2024-07-24 11:08:41 +0900

    fix(#S11P12D109-189): 파일 이름 변경 예매를 reservation으로 통일했습니다

--- Colorpl-Android/presentation/src/main/java/com/presentation/ticket/TicketFragment.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/ticket/TicketFragment.kt
@@ -2,9 +2,10 @@ package com.presentation.ticket
 
 import com.colorpl.presentation.R
 import com.colorpl.presentation.databinding.FragmentTicketBinding
+import com.presentation.base.BaseDialogFragment
 import com.presentation.base.BaseFragment
 
-class TicketFragment: BaseFragment<FragmentTicketBinding>(R.layout.fragment_ticket) {
+class TicketFragment : BaseDialogFragment<FragmentTicketBinding>(R.layout.fragment_ticket) {
     override fun initView() {
 
     }
