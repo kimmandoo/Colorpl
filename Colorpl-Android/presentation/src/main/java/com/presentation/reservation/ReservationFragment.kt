a965188f9ad7dc13de0dcdb0efaf791ebfb23448
김민규 <mingyu5675@gmail.com>
2024-07-26 00:33:10 +0900

    chore: core에서 api key 끌어오기

--- Colorpl-Android/presentation/src/main/java/com/presentation/reservation/ReservationFragment.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/reservation/ReservationFragment.kt
@@ -3,9 +3,19 @@ package com.presentation.reservation
 import com.colorpl.presentation.R
 import com.colorpl.presentation.databinding.FragmentReservationBinding
 import com.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Named
 
+@AndroidEntryPoint
 class ReservationFragment: BaseFragment<FragmentReservationBinding>(R.layout.fragment_reservation) {
-    override fun initView() {
 
+    @Inject
+    @Named("bootpay")
+    lateinit var bootpayKey: String
+
+    override fun initView() {
+        Timber.d("$bootpayKey")
     }
 }
