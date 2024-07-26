415b3c675acebbbf275142243c89eea671e67f43
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-07-26 10:22:49 +0900

    ui(#S11P12D109-198) : 환경설정 ui 완료

---
+++ Colorpl-Android/presentation/src/main/java/com/presentation/my_page/SettingFragment.kt
@@ -0,0 +1,14 @@
+package com.presentation.my_page
+
+import com.colorpl.presentation.R
+import com.colorpl.presentation.databinding.FragmentSettingBinding
+import com.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+
+    override fun initView() {
+
+    }
+}
