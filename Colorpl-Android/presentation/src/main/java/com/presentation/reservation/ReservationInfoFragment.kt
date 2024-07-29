e192a807325350ce1ab32c5b5288cf4a5487afff
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-07-29 12:07:05 +0900

    ui(#S11P12D109-183) : 결제 수단 등록 UI 완료

---
+++ Colorpl-Android/presentation/src/main/java/com/presentation/reservation/ReservationInfoFragment.kt
@@ -0,0 +1,14 @@
+package com.presentation.reservation
+
+import com.colorpl.presentation.R
+import com.colorpl.presentation.databinding.FragmentReservationInfoBinding
+import com.presentation.base.BaseFragment
+
+
+class ReservationInfoFragment : BaseFragment<FragmentReservationInfoBinding>(R.layout.fragment_reservation_info) {
+
+    override fun initView() {
+
+
+    }
+}
