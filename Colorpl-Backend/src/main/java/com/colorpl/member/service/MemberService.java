c347a12a2bc2cef8f756eb52fc197c1ff21a623d
WOONBE <kepull2918@naver.com>
2024-07-29 09:46:45 +0900

    feat(#S11P12D109-232) : Member Service 구현

---
+++ Colorpl-Backend/src/main/java/com/colorpl/member/service/MemberService.java
@@ -0,0 +1,54 @@
+package com.colorpl.member.service;
+
+import com.colorpl.global.config.TokenProvider;
+import com.colorpl.member.Member;
+import com.colorpl.member.dto.MemberDTO;
+import com.colorpl.member.dto.SignInResponse;
+import com.colorpl.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    //등록시 이메일 중복 여부 체크하는 로직 추후에 작성
+    @Transactional
+    public Member registerMember(MemberDTO memberDTO) {
+        Member member = Member.toMember(memberDTO, passwordEncoder);
+        return memberRepository.save(member);
+    }
+
+    @Transactional
+    public SignInResponse signIn(String email, String password) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("이메일이나 패스워드가 일치하지 않습니다."));
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new IllegalArgumentException("이메일이나 패스워드가 일치하지 않습니다.");
+        }
+
+        String token = tokenProvider.createToken(email);
+
+        // SignInResponse 생성 및 반환
+        return new SignInResponse(member.getNickname(), member.getType(), token);
+    }
+    @Transactional
+    public Member updateMemberInfo(Integer memberId, MemberDTO memberDTO) {
+        Member existingMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 멤버입니다."));
+
+        existingMember.updateMember(Member.toMember(memberDTO, passwordEncoder), passwordEncoder);
+        return memberRepository.save(existingMember);
+    }
+    @Transactional
+    public List<Member> getAllUsers() {
+        return memberRepository.findAll();
+    }
+}
