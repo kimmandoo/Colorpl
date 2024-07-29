81b25535c8b95d6ae87bfbe6d7ec2a1aff1f718f
박민희 <minuteman0212@naver.com>
2024-07-29 14:34:17 +0900

    fix: 테스트 오류 해결

--- Colorpl-Backend/src/test/java/com/colorpl/ColorplApplicationTests.java
+++ Colorpl-Backend/src/test/java/com/colorpl/ColorplApplicationTests.java
@@ -1,9 +1,8 @@
 package com.colorpl;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ColorplApplicationTests {
 
     @Test
