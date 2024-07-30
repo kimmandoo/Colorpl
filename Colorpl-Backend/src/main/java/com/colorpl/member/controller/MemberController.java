04f155d40708cef30ea5465517a8b32323ce4230
WOONBE <kepull2918@naver.com>
2024-07-30 14:40:21 +0900

    feat(#S11P12D109-240) : BlackList를 사용하여 JWT 로그아웃 구현

--- Colorpl-Backend/src/main/java/com/colorpl/member/controller/MemberController.java
+++ Colorpl-Backend/src/main/java/com/colorpl/member/controller/MemberController.java
@@ -4,7 +4,9 @@ import com.colorpl.member.Member;
 import com.colorpl.member.dto.MemberDTO;
 import com.colorpl.member.dto.SignInRequest;
 import com.colorpl.member.dto.SignInResponse;
+import com.colorpl.member.service.BlackListService;
 import com.colorpl.member.service.MemberService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +26,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+    private final BlackListService blackListService;
 
     @Autowired
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, BlackListService blackListService) {
         this.memberService = memberService;
+        this.blackListService = blackListService;
     }
 
     @PostMapping("/register")
@@ -55,6 +60,31 @@ public class MemberController {
                 .map(MemberDTO::toMemberDTO)
                 .collect(Collectors.toList());
         return ResponseEntity.ok(memberDTOs);
+    }
+//    @PostMapping("/sign-out")
+//    public ResponseEntity<String> signOut(@RequestHeader("Authorization") String accessToken,
+//        @RequestBody String refreshToken) {
+//        try {
+//            // Bearer 토큰 앞부분 제거
+//            String token = accessToken.replace("Bearer ", "");
+//
+//            // 로그아웃 처리
+//            blackListService.signOut(refreshToken, token);
+//
+//            return ResponseEntity.ok("로그아웃 되었습니다.");
+//        } catch (JsonProcessingException e) {
+//            return ResponseEntity.badRequest().body("Invalid token");
+//        }
+//    }
+    @PostMapping("/sign-out")
+    @PreAuthorize("hasAuthority('USER')")
+    public ResponseEntity<Void> signOut(@RequestHeader("Refresh") String refreshToken) {
+        try {
+            blackListService.signOut(refreshToken);
+            return ResponseEntity.noContent().build();
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
