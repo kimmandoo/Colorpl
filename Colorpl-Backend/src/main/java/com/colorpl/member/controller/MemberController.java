10c4f18934da8221e866b5dc543a3e7b288fb825
WOONBE <kepull2918@naver.com>
2024-07-30 10:37:21 +0900

    chore(#S11P12D109-234) : Jwt Token 코드 수정

--- Colorpl-Backend/src/main/java/com/colorpl/member/controller/MemberController.java
+++ Colorpl-Backend/src/main/java/com/colorpl/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class MemberController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAuthority('USER')")
     public ResponseEntity<List<MemberDTO>> retrieveAllMembers() {
         List<Member> members = memberService.getAllUsers();
         List<MemberDTO> memberDTOs = members.stream()
