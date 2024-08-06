3d62f2ec1ebf587fb971ed8ddf5329c615146526
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-08-06 18:02:11 +0900

    feat(#) : 회원가입 로직 푸시

--- Colorpl-Android/domain/src/main/java/com/domain/model/Member.kt
+++ Colorpl-Android/domain/src/main/java/com/domain/model/Member.kt
@@ -1,14 +1,15 @@
 package com.domain.model
 
 data class Member(
-    val email: String= "",
-    val password : String="",
-    val nickName : String ?="",
-    val type : String ="",
-    val profileImage : String ?="",
-    val accessToken: String ="",
-    val refreshToken: String ="",
-    val followerCount : Int = 0,
-    val followingCount : Int = 0,
-    val id : Int = 0
+    val email: String = "",
+    val password: String = "",
+    val nickName: String? = "",
+    val type: String = "",
+    val profileImage: String? = "",
+    val accessToken: String = "",
+    val refreshToken: String = "",
+    val followerCount: Int = 0,
+    val followingCount: Int = 0,
+    val id: Int = 0,
+    val categories: List<String> = emptyList()
 )
