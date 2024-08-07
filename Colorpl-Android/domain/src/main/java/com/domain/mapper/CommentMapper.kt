4a8a65d4e71f546ed6584a5c7ccf70ed689ea71f
Mingyu Kim <mingyu5675@gmail.com>
2024-08-07 09:50:23 +0900

    feat(#S11P12D109-325): 코멘트 crud api 연결

--- Colorpl-Android/domain/src/main/java/com/domain/mapper/CommentMapper.kt
+++ Colorpl-Android/domain/src/main/java/com/domain/mapper/CommentMapper.kt
@@ -1,14 +1,14 @@
 package com.domain.mapper
 
 import com.data.model.paging.Comment
-import com.data.model.paging.Feed
 
 fun Comment.toEntity(): com.domain.model.Comment {
     return com.domain.model.Comment(
-        commentId = commentId,
-        name = name,
-        uploadDate = uploadDate,
-        lastEditDate = lastEditDate,
-        content = content
+        id = id,
+        reviewId = reviewId,
+        memberId = memberId,
+        writer = writer,
+        commentContent = commentContent,
+        createdate = createdate
     )
 }
