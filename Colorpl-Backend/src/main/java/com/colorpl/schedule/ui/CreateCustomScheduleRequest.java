f0ccf957a3d2059936dc39904b34b4439dbc9cf1
박민희 <minuteman0212@naver.com>
2024-08-07 14:15:51 +0900

    fix(#S11P12D109-373): 일정 등록 기능 파라미터 바인딩 오류 수정

--- Colorpl-Backend/src/main/java/com/colorpl/schedule/ui/CreateCustomScheduleRequest.java
+++ Colorpl-Backend/src/main/java/com/colorpl/schedule/ui/CreateCustomScheduleRequest.java
@@ -1,6 +1,7 @@
 package com.colorpl.schedule.ui;
 
 import com.colorpl.show.domain.detail.Category;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Getter;
 
@@ -8,6 +9,7 @@ import lombok.Getter;
 public class CreateCustomScheduleRequest {
 
     private String seat;
+    @JsonFormat(pattern = "yyyy년 MM월 dd일 HH:mm")
     private LocalDateTime dateTime;
     private String name;
     private Category category;
