8c0b3515d01a80c09f2a81ae43c0920e485c9647
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-08-08 12:05:40 +0900

    feat(#S11P12D109-74) : 결제 요청 API

--- Colorpl-Android/data/src/main/java/com/data/api/PayApi.kt
+++ Colorpl-Android/data/src/main/java/com/data/api/PayApi.kt
@@ -1,6 +1,19 @@
 package com.data.api
 
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface PayApi {
 
+    @GET("api/payment/token")
     suspend fun getPaymentToken() : String
+
+
+    @POST("api/payment/confirm")
+    suspend fun postPayment(
+        @Header("Authorization") Authorization : String,
+        @Query("receiptId") receiptId : String
+    ) : 
 }
