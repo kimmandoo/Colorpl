5a88bc65fb8988b6698ba3c5058c0891e7acc81b
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-08-08 16:23:39 +0900

    feat(#S11P12D109-348) : 뒤로가기

--- Colorpl-Android/presentation/src/main/java/com/presentation/my_page/OtherMyPageFragment.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/my_page/OtherMyPageFragment.kt
@@ -1,5 +1,6 @@
 package com.presentation.my_page
 
+import androidx.core.view.postDelayed
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
@@ -7,8 +8,11 @@ import androidx.navigation.fragment.navArgs
 import com.colorpl.presentation.R
 import com.colorpl.presentation.databinding.FragmentOtherMyPageBinding
 import com.presentation.base.BaseFragment
+import com.presentation.my_page.model.MyPageEventState
 import com.presentation.my_page.model.OtherMyPageEventState
+import com.presentation.util.setDistanceX
 import com.presentation.util.setImageCircleCrop
+import com.presentation.util.setTransactionX
 import com.presentation.viewmodel.MyPageViewModel
 import com.presentation.viewmodel.OtherMyPageViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -70,10 +74,52 @@ class OtherMyPageFragment :
                 }
             }
             .launchIn(viewLifecycleOwner.lifecycleScope)
+
+        myPageViewModel.myPageEventState.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                when (it) {
+                    is MyPageEventState.UseTicket -> {
+                        binding.apply {
+                            ticketTitle = true
+                            ticketCount = it.data.size.toString()
+                            ivTicketExpire.isSelected = true
+                            ivTicketStar.isSelected = false
+                            val distance =
+                                setDistanceX(binding.ivTicketStar, binding.ivTicketExpire)
+                            indicator.setTransactionX(distance)
+                        }
+
+                    }
+
+                    is MyPageEventState.UnUseTicket -> {
+                        binding.apply {
+                            ticketTitle = false
+                            ticketCount = it.data.size.toString()
+                            ivTicketStar.isSelected = true
+                            ivTicketExpire.isSelected = false
+                            indicator.setTransactionX(0f)
+                        }
+                    }
+                }
+            }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
     }
 
     private fun initClickEvent() {
         binding.apply {
+            ivBack.setOnClickListener {
+                navigatePopBackStack()
+            }
+            ivTicketStar.postDelayed(300L) {
+                binding.ivTicketStar.performClick()
+            }
+            ivTicketStar.setOnClickListener {
+                myPageViewModel.ticketEvent(UN_USE)
+
+            }
+            ivTicketExpire.setOnClickListener {
+                myPageViewModel.ticketEvent(USE)
+            }
             tvFollowRequest.setOnClickListener {
                 if (!binding.tvFollowRequest.isSelected) {
                     otherMyPageViewModel.follow()
@@ -86,4 +132,8 @@ class OtherMyPageFragment :
 
     }
 
+    companion object {
+        const val USE = true
+        const val UN_USE = false
+    }
 }
