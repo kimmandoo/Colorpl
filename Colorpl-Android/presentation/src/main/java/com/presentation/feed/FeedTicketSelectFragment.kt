d60a74c7e83cec8f33477cce270b07b34cd2d5e0
Mingyu Kim <mingyu5675@gmail.com>
2024-08-08 17:33:27 +0900

    feat(#S11P12D109-326): ticket select 화면 구성중

--- Colorpl-Android/presentation/src/main/java/com/presentation/feed/FeedTicketSelectFragment.kt
+++ Colorpl-Android/presentation/src/main/java/com/presentation/feed/FeedTicketSelectFragment.kt
@@ -1,13 +1,18 @@
 package com.presentation.feed
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
 import com.colorpl.presentation.R
 import com.colorpl.presentation.databinding.FragmentFeedTicketSelectBinding
 import com.presentation.base.BaseDialogFragment
 import com.presentation.component.adapter.feed.FeedTicketSelectAdapter
 import com.presentation.util.addCustomItemDecoration
+import com.presentation.viewmodel.TicketSelectViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 
@@ -15,7 +20,12 @@ import timber.log.Timber
 class FeedTicketSelectFragment :
     BaseDialogFragment<FragmentFeedTicketSelectBinding>(R.layout.fragment_feed_ticket_select) {
 
-    private lateinit var feedTicketSelectAdapter: FeedTicketSelectAdapter
+    private val feedTicketSelectAdapter by lazy {
+        FeedTicketSelectAdapter() {
+
+        }
+    }
+    private val viewModel: TicketSelectViewModel by viewModels()
 
     //얘도 ViewModel에서 따로 빼서 쓰면 될듯
     var ticketPosition = 0
@@ -23,11 +33,21 @@ class FeedTicketSelectFragment :
     override fun initView(savedInstanceState: Bundle?) {
         initAdapter()
         initClickEvent()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            launch {
+                viewModel.tickets.collectLatest { unreviewedList ->
+                    feedTicketSelectAdapter.submitList(unreviewedList)
+                }
+            }
+        }
     }
 
 
     private fun initAdapter() {
-        feedTicketSelectAdapter = FeedTicketSelectAdapter()
         binding.vpFeedTicketSelect.apply {
             adapter = feedTicketSelectAdapter
             offscreenPageLimit = 1
@@ -38,7 +58,6 @@ class FeedTicketSelectFragment :
                 }
             })
         }
-        feedTicketSelectAdapter.submitList(listOf("", "", "", "", ""))
     }
 
     private fun initClickEvent() {
