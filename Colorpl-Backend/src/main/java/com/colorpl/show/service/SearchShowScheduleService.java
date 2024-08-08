94444aff791880654b8e12b2003a7b99b9d882b7
박민희 <minuteman0212@naver.com>
2024-08-08 16:50:26 +0900

    feat(#S11P12D109-379): 공연일정 조회 기능 구현

---
+++ Colorpl-Backend/src/main/java/com/colorpl/show/service/SearchShowScheduleService.java
@@ -0,0 +1,64 @@
+package com.colorpl.show.service;
+
+import com.colorpl.show.domain.detail.ShowDetail;
+import com.colorpl.show.dto.HallListResponse;
+import com.colorpl.show.dto.SearchShowScheduleCondition;
+import com.colorpl.show.dto.ShowScheduleListItem;
+import com.colorpl.show.dto.TheaterListResponse;
+import com.colorpl.show.repository.ShowDetailRepository;
+import com.colorpl.show.repository.ShowScheduleRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SearchShowScheduleService {
+
+    private final ShowDetailRepository showDetailRepository;
+    private final ShowScheduleRepository showScheduleRepository;
+
+    @Value("${seat.rows}")
+    private int rows;
+    @Value("${seat.cols}")
+    private int cols;
+
+    public TheaterListResponse search(Long showDetailId, LocalDate date) {
+
+        ShowDetail showDetail = showDetailRepository.findById(showDetailId).orElseThrow();
+        LocalDateTime from = date.atStartOfDay();
+        LocalDateTime to = from.plusDays(1);
+
+        SearchShowScheduleCondition condition = SearchShowScheduleCondition.builder()
+            .showDetail(showDetail)
+            .from(from)
+            .to(to)
+            .build();
+
+        List<ShowScheduleListItem> timetable = showScheduleRepository.search(condition).stream()
+            .map(showSchedule -> {
+
+                // 남은 좌석 수
+
+                return ShowScheduleListItem.builder()
+                    .startTime(showSchedule.getDateTime())
+                    .endTime(showSchedule.getDateTime())
+                    .build();
+            })
+            .toList();
+
+        HallListResponse hallListResponse = HallListResponse.builder()
+            .name(showDetail.getHall())
+            .countSeats(rows * cols)
+            .timetable(timetable)
+            .build();
+
+        return TheaterListResponse.builder()
+            .name(null)
+            .hall(hallListResponse)
+            .build();
+    }
+}
