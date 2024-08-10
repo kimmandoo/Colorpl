d1c43e721b9ee457953de1ce06cc1b290f3977be
박민희 <minuteman0212@naver.com>
2024-08-11 04:21:25 +0900

    feat(#S11P12D109-390): 예매현황 생성 기능 구현

---
+++ Colorpl-Backend/src/main/java/com/colorpl/reservation/status/service/CreateReservationStatusService.java
@@ -0,0 +1,44 @@
+package com.colorpl.reservation.status.service;
+
+import com.colorpl.reservation.status.domain.ReservationStatus;
+import com.colorpl.reservation.status.repository.ReservationStatusRepository;
+import com.colorpl.show.domain.Seat;
+import java.util.stream.IntStream;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateReservationStatusService {
+
+    @Value("${seat.rows}")
+    private int rows;
+
+    @Value("${seat.cols}")
+    private int cols;
+
+    private final ReservationStatusRepository reservationStatusRepository;
+
+    public void createReservationStatus(Long showScheduleId) {
+        reservationStatusRepository.findById(showScheduleId).orElseGet(() -> {
+            ReservationStatus reservationStatus = ReservationStatus.builder()
+                .showScheduleId(showScheduleId)
+                .build();
+            createReserved(reservationStatus);
+            return reservationStatusRepository.save(reservationStatus);
+        });
+    }
+
+    private void createReserved(ReservationStatus reservationStatus) {
+        IntStream.rangeClosed(0, rows)
+            .forEach(i -> IntStream.rangeClosed(0, cols)
+                .forEach(j -> {
+                    Seat seat = Seat.builder()
+                        .row(i)
+                        .col(j)
+                        .build();
+                    reservationStatus.getReserved().put(seat.toString(), true);
+                }));
+    }
+}
---
+++ Colorpl-Backend/src/test/java/com/colorpl/reservation/status/service/CreateReservationStatusServiceTest.java
@@ -0,0 +1,42 @@
+package com.colorpl.reservation.status.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.colorpl.reservation.status.domain.ReservationStatus;
+import com.colorpl.reservation.status.repository.ReservationStatusRepository;
+import com.colorpl.show.domain.Seat;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateReservationStatusServiceTest {
+
+    @Autowired
+    private ReservationStatusRepository reservationStatusRepository;
+    @Autowired
+    private CreateReservationStatusService createReservationStatusService;
+    private Seat seat;
+
+    @BeforeEach
+    void setUp() {
+        seat = Seat.builder()
+            .row(0)
+            .col(0)
+            .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        reservationStatusRepository.deleteAll();
+    }
+
+    @Test
+    void createReservationStatus() {
+        createReservationStatusService.createReservationStatus(0L);
+        ReservationStatus status = reservationStatusRepository.findById(0L).orElseThrow();
+        assertThat(status.getReserved().get(seat.toString())).isEqualTo(true);
+    }
+}
