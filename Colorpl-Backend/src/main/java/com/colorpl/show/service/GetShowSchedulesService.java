43afce95fc5bf3e1d3f3d98badca7903d58e8a8f
박민희 <minuteman0212@naver.com>
2024-08-13 09:17:10 +0900

    fix(#S11P12D109-457): 공연일정 목록 조회 기능 런타임 파싱 오류 수정

--- Colorpl-Backend/src/main/java/com/colorpl/show/service/GetShowSchedulesService.java
+++ Colorpl-Backend/src/main/java/com/colorpl/show/service/GetShowSchedulesService.java
@@ -16,6 +16,7 @@ import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -30,41 +31,40 @@ public class GetShowSchedulesService {
 
     public List<GetShowSchedulesResponse> getShowSchedules(Integer showDetailId, LocalDate date) {
         GetShowSchedulesRequest request = GetShowSchedulesRequest.builder()
-            .showDetailId(showDetailId)
-            .date(date)
-            .build();
+            .showDetailId(showDetailId).date(date).build();
         ShowDetail showDetail = showDetailRepository.getShowSchedules(request);
-        Hall hall = Hall.builder()
-            .name(showDetail.getHall().getName())
-            .countSeats(rows * cols)
+        Hall hall = Hall.builder().name(showDetail.getHall().getName()).countSeats(rows * cols)
             .build();
-        showDetail.getShowSchedules()
-            .forEach(showSchedule -> {
-                Integer remainingSeats = (int) getReservationStatusService.getReservationStatus(
-                        showSchedule.getId()).getReserved().values().stream()
-                    .filter(item -> item.getIsReserved().equals(false)).count();
-                Timetable timetable = Timetable.builder()
-                    .showScheduleId(showSchedule.getId())
-                    .startTime(showSchedule.getDateTime())
-                    .endTime(showSchedule.getDateTime()
-                        .plus(parse(showSchedule.getShowDetail().getRuntime())))
-                    .remainingSeats(remainingSeats)
-                    .build();
-                hall.getTimetable().add(timetable);
-            });
+        showDetail.getShowSchedules().forEach(showSchedule -> {
+            Integer remainingSeats = (int) getReservationStatusService.getReservationStatus(
+                    showSchedule.getId()).getReserved().values().stream()
+                .filter(item -> item.getIsReserved().equals(false)).count();
+            Timetable timetable = Timetable.builder().showScheduleId(showSchedule.getId())
+                .startTime(showSchedule.getDateTime()).endTime(showSchedule.getDateTime()
+                    .plus(parse(showSchedule.getShowDetail().getRuntime())))
+                .remainingSeats(remainingSeats).build();
+            hall.getTimetable().add(timetable);
+        });
         GetShowSchedulesResponse response = GetShowSchedulesResponse.builder()
-            .name(showDetail.getHall().getTheater().getName())
-            .build();
+            .name(showDetail.getHall().getTheater().getName()).build();
         response.getHall().add(hall);
         return List.of(response);
     }
 
     private static Duration parse(String source) {
-        Pattern pattern = Pattern.compile("(\\d+)시간\\s+(\\d+)분");
+        Pattern pattern = Pattern.compile("(?:(\\d+)시간)?\\s*(?:(\\d+)분)?");
         Matcher matcher = pattern.matcher(source);
         if (!matcher.find()) {
             throw new InvalidRuntimeException(source);
         }
-        return Duration.parse("PT%sH%sM".formatted(matcher.group(1), matcher.group(2)));
+        String hours = matcher.group(1);
+        String minutes = matcher.group(2);
+        if (StringUtils.hasText(hours)) {
+            if (StringUtils.hasText(minutes)) {
+                return Duration.parse("PT%sH%sM".formatted(hours, minutes));
+            }
+            return Duration.parse("PT%sH".formatted(hours));
+        }
+        return Duration.parse("PT%sM".formatted(minutes));
     }
 }
