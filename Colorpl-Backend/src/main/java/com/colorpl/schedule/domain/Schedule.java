3c223c06b106c39125b32612c8493c7bd4b9ec58
WOONBE <kepull2918@naver.com>
2024-08-13 17:36:06 +0900

    feat(#S11P12D109-411) : 예매와 결제 및 티켓 생성 통합

--- Colorpl-Backend/src/main/java/com/colorpl/schedule/domain/Schedule.java
+++ Colorpl-Backend/src/main/java/com/colorpl/schedule/domain/Schedule.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
@@ -26,7 +27,7 @@ import lombok.NoArgsConstructor;
 public abstract class Schedule {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SCHEDULE_ID")
     private Long id;
 
