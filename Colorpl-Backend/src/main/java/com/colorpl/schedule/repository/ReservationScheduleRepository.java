024da9147cf98e1b1d51bbe4a9339d1ed35185d0
박민희 <minuteman0212@naver.com>
2024-08-14 16:17:04 +0900

    feat(#S11P12D109-459): 티켓 결제 취소 시 일정 삭제 기능 구현

--- Colorpl-Backend/src/main/java/com/colorpl/schedule/repository/ReservationScheduleRepository.java
+++ Colorpl-Backend/src/main/java/com/colorpl/schedule/repository/ReservationScheduleRepository.java
@@ -1,9 +1,12 @@
 package com.colorpl.schedule.repository;
 
+import com.colorpl.reservation.domain.Reservation;
 import com.colorpl.schedule.domain.ReservationSchedule;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationScheduleRepository extends JpaRepository<ReservationSchedule, Long>,
     ReservationScheduleRepositoryCustom {
 
+    Optional<ReservationSchedule> findByReservation(Reservation reservation);
 }
